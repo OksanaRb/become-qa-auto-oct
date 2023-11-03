02426a6d78913ddc59037bedf35ab6a2e8e0886a
Oksana Rabchevska <oksana.rabchevska@globallogic>
2023-11-03 09:32:49 +0200

    Added github api client

--- tests/github/api/test_api.py
+++ tests/github/api/test_api.py
@@ -1,16 +1,29 @@
-from src.config.config_easy import ConfigEasy
-from src.config.config_harder import config
+from src.applications.github.api.github_api import GitHubAPIClient
 
-def test_api():
-    print(ConfigEasy.request_timeout)
-    print(config.BASE_URL_UI)
+def test_search_for_existing_repo():
+    github_api_client = GitHubAPIClient()
+    existing_repo_name = 'oksana'
+    repos = github_api_client.search_repos(existing_repo_name) # list/array returned 
 
-def test_api():
-    print(ConfigEasy.request_timeout)
+    print("Checking total count is not 0")
+    assert repos['total_count'] != 0  # some elements exist in array/list
 
-def test_api():
-    print(ConfigEasy.request_timeout)
 
-def test_api():
-    print(ConfigEasy.request_timeout)
+def test_search_for_nonexisting_repo():
+    github_api_client = GitHubAPIClient()
+    nonexisting_repo_name = 'nvbfhgb'
+    repos = github_api_client.search_repos(nonexisting_repo_name) # list/array returned 
+
+    print("Checking total count is 0")
+    assert repos['total_count'] == 0  # some elements exist in array/list
+
+
+def test_search_for_nonexisting_repo():
+    github_api_client = GitHubAPIClient()
+    nonexisting_commit_hash = 'nvbfhgb'
+    repos = github_api_client.search_commits(nonexisting_commit_hash) # list/array returned 
+
+    print("Checking total count is 0")
+    assert 0 == 0  # some elements exist in array/list
+  
 
