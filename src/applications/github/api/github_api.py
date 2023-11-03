9b1367c1b5b7609c366d7bec304bfb1ac286fbdb
Oksana Rabchevska <oksana.rabchevska@globallogic>
2023-11-03 09:52:53 +0200

    last commit

--- src/applications/github/api/github_api.py
+++ src/applications/github/api/github_api.py
@@ -1,5 +1,27 @@
-class GitHubAPI:
+import requests
+
+
+class GitHubAPIClient:
     """Current class contains all API call we use in tests"""
     
     def __init__(self) -> None:
         pass
+
+    def search_repos(self, repo_name):
+        print("Sending request to url https://api.github.com/search/repositories")
+        r = requests.get("https://api.github.com/search/repositories", params={'q' : repo_name})
+        print (f"Responce retriewed {r}")
+
+        body = r.json()
+        
+        return body
+
+
+    def search_commits(self, commit_hash):
+        print("Sending request to url https://api.github.com/search/commits")
+        r = requests.get("https://api.github.com/search/commits", params={'q' : commit_hash})
+        print (f"Responce retriewed {r}")
+
+        body = r.json()
+        
+        return body
